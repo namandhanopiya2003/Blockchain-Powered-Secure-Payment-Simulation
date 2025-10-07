b853d67595cd7af5f12462afef80fff13772e128
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-10-07 14:01:43 +0530

    Improved code readability

--- Blockchain-Powered Secure Payment Simulation/migrations/2_deploy_contracts.js
+++ Blockchain-Powered Secure Payment Simulation/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+// Loads the compiled Payroll smart contract
 const Payroll = artifacts.require("Payroll");
 
+// This function runs when the contract is being deployed
 module.exports = function (deployer) {
+  // Deploys the Payroll contract to the blockchain
   deployer.deploy(Payroll);
 };
+
