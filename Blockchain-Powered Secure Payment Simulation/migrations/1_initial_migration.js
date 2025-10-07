7695182fedf0cba20d3d6a0b3bda286f1a0143ef
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-10-07 13:59:23 +0530

    Added comments

--- Blockchain-Powered Secure Payment Simulation/migrations/1_initial_migration.js
+++ Blockchain-Powered Secure Payment Simulation/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
+// This line loads the compiled "Migrations" smart contract
 const Migrations = artifacts.require("Migrations");
 
+// This is the main function that runs when it deploy the contracts
 module.exports = function (deployer) {
+  // This line tells to deploy the Migrations contract to the blockchain
   deployer.deploy(Migrations);
 };
+
