70fc3fbcc0ca48750b1d15c5cfe3375df40dde19
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-10-08 09:34:25 +0530

    Updated documentation

--- Blockchain-Powered Secure Payment Simulation/truffle-config.js
+++ Blockchain-Powered Secure Payment Simulation/truffle-config.js
@@ -1,14 +1,19 @@
 module.exports = {
   networks: {
     development: {
+      // Localhost IP address (own device)
       host: "127.0.0.1",
+      // This is the port Ethereum app will use
       port: 8545,
+      // It connects to network ID
       network_id: "*",
     },
   },
   compilers: {
     solc: {
+      // Version of the Solidity language used to compile contracts
       version: "0.8.17",
     },
   },
 };
+
